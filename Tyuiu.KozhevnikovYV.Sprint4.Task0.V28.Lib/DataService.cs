14f4497c4de272a434ea9126f9a5366acf404fb7
KozhevnikovYaroslav <nekit13012001nikita@gmail.com>
2025-11-08 21:21:27 +0500

    Add project files.

---
+++ Tyuiu.KozhevnikovYV.Sprint4.Task0.V28.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿namespace Tyuiu.KozhevnikovYV.Sprint4.Task0.V28.Lib
+{
+    using tyuiu.cources.programming.interfaces.Sprint4;
+    public class DataService : ISprint4Task0V28
+    {
+        public int GetMultEvenArrEl(int[] array)
+        {
+            int p = 1;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 == 0)
+                {
+                    p *= array[i];
+                }
+            }
+            return p;
+        }
+    }
+}
---
+++ Tyuiu.KozhevnikovYV.Sprint4.Task0.V28.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿namespace Tyuiu.KozhevnikovYV.Sprint4.Task0.V28.Test
+{ using Tyuiu.KozhevnikovYV.Sprint4.Task0.V28.Lib;
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[] array = { 9, 8, 4, 6, 9, 4, 3, 6, 1, 2 };
+            int res = ds.GetMultEvenArrEl(array);
+            int wait = 9216;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
