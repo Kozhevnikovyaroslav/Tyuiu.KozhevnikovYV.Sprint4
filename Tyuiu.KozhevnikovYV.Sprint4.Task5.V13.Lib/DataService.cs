84074ebdf7f7aa7177507f51322d9301eb886656
KozhevnikovYaroslav <nekit13012001nikita@gmail.com>
2025-11-09 19:19:35 +0500

    Task5

---
+++ Tyuiu.KozhevnikovYV.Sprint4.Task5.V13.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿namespace Tyuiu.KozhevnikovYV.Sprint4.Task5.V13.Lib
+{
+    using tyuiu.cources.programming.interfaces.Sprint4;
+    public class DataService : ISprint4Task5V13
+    {
+        public int[,] Calculate(int[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int colums = matrix.GetLength(1);
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < colums; j++)
+                {
+                    if (matrix[i, j] < 0)
+                    {
+                        matrix[i, j] = 0;
+                    }
+                }
+            }
+            return matrix;
+        }
+    }
+}
---
+++ Tyuiu.KozhevnikovYV.Sprint4.Task5.V13.Test/DataServiceTest.cs
@@ -0,0 +1,26 @@
+﻿namespace Tyuiu.KozhevnikovYV.Sprint4.Task5.V13.Test
+{
+    using Tyuiu.KozhevnikovYV.Sprint4.Task5.V13.Lib;
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[,] mtrx = new int[5, 5] { { -2, -2, -2, -2, -2 },
+                                          { -2, -2, -2, -2, -2 },
+                                          { -2, -2, 2, -2, -2 },
+                                          { -2, -2, -2, -2, -2 },
+                                          { -2, -2, -2, -2, -2 } };
+
+            int[,] wait = new int[5, 5] { { 0, 0, 0, 0, 0 },
+                                          { 0, 0, 0, 0, 0 },
+                                          { 0, 0, 2, 0, 0 },
+                                          { 0, 0, 0, 0, 0 },
+                                          { 0, 0, 0, 0, 0 } };
+            int[,] res = ds.Calculate(mtrx);
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
